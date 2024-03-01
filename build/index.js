637f011014b5c94befed18b6c33733c91dd7a1cd
JoaoAndrade <joaopaulo.arteagle17@gmail.com>
2024-03-01 14:51:35 -0300

    First commit

---
+++ build/index.js
@@ -0,0 +1,89 @@
+"use strict";
+let taskName = document.querySelector("#taskName");
+let resp = document.querySelector("#resp");
+let table = document.getElementById("#table");
+let tbody = document.querySelector("#tbody");
+let button = document.querySelector("#taskBox button");
+class Tarefa {
+    constructor(name, resp, status) {
+        this.name = name;
+        this.resp = resp;
+        this.status = status;
+    }
+}
+let listaSalva = localStorage.getItem("@listagem_task");
+let tarefas = listaSalva !== null && JSON.parse(listaSalva) || [];
+function checkDo(pos) {
+    tarefas[pos].status = !tarefas[pos].status;
+    if (tarefas[pos].status) {
+        alert("entrou");
+        listar();
+        saveData();
+        return true;
+    }
+    else {
+        alert("saiu");
+        listar();
+        saveData();
+        return false;
+    }
+}
+function listar() {
+    //limpar o corpo da tabela
+    tbody.innerHTML = "";
+    tarefas.forEach(item => {
+        const tr = document.createElement("tr");
+        const tdCheck = document.createElement("td");
+        tdCheck.setAttribute("style", "width: 5px;border:none");
+        const check = document.createElement("input");
+        check.setAttribute("type", "checkbox");
+        check.setAttribute("onchange", `checkDo(${tarefas.indexOf(item)})`);
+        check.setAttribute("style", "width: 8vh");
+        tdCheck.appendChild(check);
+        const tdTask = document.createElement("td");
+        tdTask.textContent = item.name;
+        const tdResp = document.createElement("td");
+        tdResp.textContent = item.resp;
+        const tdA = document.createElement("td");
+        const linkElement = document.createElement("a");
+        linkElement.setAttribute("href", "#");
+        linkElement.setAttribute("onclick", `deletar(${tarefas.indexOf(item)})`);
+        const lixeira = document.createElement("img");
+        lixeira.setAttribute("src", "./img/lixeira.png");
+        linkElement.appendChild(lixeira);
+        tdA.appendChild(linkElement);
+        tr.appendChild(tdCheck);
+        tr.appendChild(tdTask);
+        tr.appendChild(tdResp);
+        tr.appendChild(tdA);
+        tbody.appendChild(tr);
+    });
+}
+;
+function deletar(pos) {
+    tarefas.splice(pos, 1);
+    listar();
+    saveData();
+}
+listar();
+function add() {
+    console.log("Função");
+    if (taskName.value === "" || resp.value === "") {
+        alert("Campo não Pode estar vazio!");
+        return false;
+    }
+    else {
+        let task1 = new Tarefa(taskName.value, resp.value, false);
+        tarefas.push(task1);
+        taskName.value = "";
+        resp.value = "";
+        listar();
+        saveData();
+    }
+}
+;
+button.onclick = add;
+function saveData() {
+    localStorage.setItem("@listagem_task", JSON.stringify(tarefas));
+}
+;
---
+++ src/index.ts
@@ -0,0 +1,128 @@
+
+let taskName = document.querySelector("#taskName") as HTMLInputElement;
+let resp = document.querySelector("#resp") as HTMLInputElement;
+
+let table = document.getElementById("#table") as HTMLTableElement;
+let tbody = document.querySelector("#tbody") as HTMLTableSectionElement;
+let button = document.querySelector("#taskBox button") as HTMLElement;
+
+    class Tarefa{
+        name: string;
+        resp:string;
+        status:boolean;
+
+        constructor(name:string, resp:string, status:boolean){
+            this.name = name;
+            this.resp = resp;
+            this.status = status;
+        }
+        
+    }
+      
+    let listaSalva:(string|null) = localStorage.getItem("@listagem_task");
+    let tarefas: Tarefa[]  = listaSalva!==null && JSON.parse(listaSalva) || []; 
+  
+    
+    function checkDo(pos:number):boolean{
+        
+        tarefas[pos].status = !tarefas[pos].status;
+
+            if(tarefas[pos].status){
+                alert("entrou");
+                listar();
+                saveData();
+               return true ;
+            }else{
+                alert("saiu");
+                listar();
+                saveData();
+                return false;
+            }
+        
+    }
+    
+     
+    
+    
+    function listar(){
+        
+        //limpar o corpo da tabela
+        tbody.innerHTML = "";
+        
+        tarefas.forEach(item =>{
+            const tr = document.createElement("tr");
+
+            const tdCheck = document.createElement("td");
+            tdCheck.setAttribute("style","width: 5px;border:none");
+            const check = document.createElement("input");
+            check.setAttribute("type","checkbox");
+            check.setAttribute("onchange", `checkDo(${tarefas.indexOf(item)})`);
+            check.setAttribute("style", "width: 8vh");
+            tdCheck.appendChild(check);
+
+            const tdTask = document.createElement("td");
+            tdTask.textContent = item.name;
+
+            const tdResp = document.createElement("td");
+            tdResp.textContent = item.resp;
+            
+            const tdA = document.createElement("td");
+            const linkElement = document.createElement("a");
+            linkElement.setAttribute("href", "#");
+            linkElement.setAttribute("onclick", `deletar(${ tarefas.indexOf(item)})`);
+            
+            const lixeira = document.createElement("img");
+            lixeira.setAttribute("src", "./img/lixeira.png")
+            linkElement.appendChild(lixeira);
+            tdA.appendChild(linkElement);
+
+            tr.appendChild(tdCheck);
+            tr.appendChild(tdTask);
+            tr.appendChild(tdResp);
+            tr.appendChild(tdA);
+
+            tbody.appendChild(tr);
+
+
+            
+        });
+
+    };
+
+    function deletar(pos:number):void{
+        tarefas.splice(pos, 1);
+        listar();
+        saveData();
+    }
+
+    listar();
+
+    function add():boolean|void{
+        console.log("Função");
+        if(taskName.value==="" || resp.value ===""){
+            alert("Campo não Pode estar vazio!");
+            return false;
+        }else{
+
+            let task1 = new Tarefa(taskName.value, resp.value, false);
+                
+
+            tarefas.push(task1);
+
+            taskName.value = "";
+            resp.value = "";
+           
+
+            listar();
+            saveData();
+        }
+
+  };
+
+
+
+  button.onclick = add;
+
+  function saveData(){
+    localStorage.setItem("@listagem_task", JSON.stringify(tarefas));
+  };
